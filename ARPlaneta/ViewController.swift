375c3f78641620e25745c352856a2405c68599db
gilrodar <gilrodar@gmail.com>
2017-12-06 22:49:51 -0600

    AR Planeta

---
+++ ARPlaneta/ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  ARPlaneta
+//
+//  Created by Gil Rodarte on 31/10/17.
+//  Copyright © 2017 Gil Rodarte. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        let scene = SCNScene()
+        
+        let position = SCNVector3(0, 0, -0.3)
+        let globe = createTierra(at: position)
+        scene.rootNode.addChildNode(globe)
+        // Set the scene to the view
+        sceneView.scene = scene
+    }
+    
+    func createTierra(at position:SCNVector3) -> SCNNode{
+        let sphere = SCNSphere(radius: 0.1)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: "art.scnassets/earth.jpeg")
+        sphere.firstMaterial = material
+        let sphereNode = SCNNode(geometry: sphere)
+        sphereNode.position = position
+        return sphereNode
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
